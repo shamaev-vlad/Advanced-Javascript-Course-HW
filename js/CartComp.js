bed170a84f01bd8a33e427cd12bb1dc9f2dbf522
Vlad Shamaev <vladshamaev@gmail.com>
2020-03-04 23:06:55 +0300

    Практическая работа по шестому уроку.

---
+++ js/CartComp.js
@@ -0,0 +1,93 @@
+Vue.component('cart', {
+    data(){
+      return {
+          imgCart: 'https://placehold.it/50x100',
+          cartUrl: '/getBasket.json',
+          cartItems: [],
+          showCart: false,
+      }
+    },
+    methods: {
+        addProduct(product){
+            this.$parent.getJson(`${API}/addToBasket.json`)
+                .then(data => {
+                    if(data.result === 1){
+                        let find = this.cartItems.find(el => el.id_product === product.id_product);
+                        if(find){
+                            find.quantity++;
+                        } else {
+                            let prod = Object.assign({quantity: 1}, product);
+                            this.cartItems.push(prod)
+                        }
+                    } else {
+                        alert('Error');
+                    }
+                })
+        },
+        remove(item) {
+            this.$parent.getJson(`${API}/deleteFromBasket.json`)
+                .then(data => {
+                    if(data.result === 1) {
+                        if(item.quantity>1){
+                            item.quantity--;
+                        } else {
+                            this.cartItems.splice(this.cartItems.indexOf(item), 1)
+                        }
+                    }
+                })
+        },
+    },
+    mounted(){
+        this.$parent.getJson(`${API + this.cartUrl}`)
+            .then(data => {
+                for(let el of data.contents){
+                    this.cartItems.push(el);
+                }
+            });
+    },
+    template: `
+        <div>
+            <button class="btn-cart" type="button" @click="showCart = !showCart">Корзина</button>
+            <div class="cart-block" v-show="showCart">
+                <p v-if="!cartItems.length">Корзина пуста</p>
+                <cart-item class="cart-item" 
+                v-for="item of cartItems" 
+                :key="item.id_product"
+                :cart-item="item" 
+                :img="imgCart"
+                @remove="remove">
+                </cart-item>
+            </div>
+        </div>`
+});
+
+Vue.component('cart-item', {
+    props: ['cartItem', 'img'],
+    template: `
+                <div class="product-cart-item">
+                    <div class="product-bio">
+                        <img :src="img" alt="Some image">
+                        <div class="product-desc">
+                            <p class="product-name">{{cartItem.product_name}}</p>
+                            <p class="product-quantity">Количество: {{cartItem.quantity}}</p>
+                            <p class="product-single-price">{{cartItem.price}}₽ за единицу</p>
+                        </div>
+                    </div>
+                    <div class="right-block">
+                        <p class="product-price">Итоговая цена: {{cartItem.quantity*cartItem.price}}₽</p>
+                        <button class="del-btn" @click="$emit('remove', cartItem)">&times;</button>
+                    </div>
+                </div>
+    `
+});
+
+// <div class="cart-block" :class="divClasses">
+// <div class="product-cart-item ">
+//     <img :src="imgCatalogSmall" alt="Some img">
+//     <div class="desc">
+//         <h3 class="product-name">Название</h3>
+//         <p class="product-price">Цена</p>
+//     </div>
+//     <button class="by-btn" @click="">Купить</button>
+// </div>
+// </div>
--- js/main.js
+++ js/main.js
@@ -3,56 +3,19 @@ const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-a
 const app = new Vue({
     el: '#app',
     data: {
-        catalogUrl: '/catalogData.json',
-        products: [],
-        imgCatalog: 'https://placehold.it/200x250',
-        imgCatalogSmall: 'https://placehold.it/100x100',
-        filteredGoods: [],
-        searchLine: '',
-        isVisible: false
-
-    },    methods: {
+        userSearch: '',
+    },
+    methods: {
         getJson(url){
             return fetch(url)
                 .then(result => result.json())
                 .catch(error => {
-                    console.log(error);
+                   this.$refs.error.activate(url);
                 })
         },
-        addProduct(product){
-            console.log(product.id_product);
-        },
-        FilterGoods() {
-            const regexp = new RegExp(this.searchLine, 'i');
-            this.filteredGoods = this.products.filter(product => {
-                return regexp.test(product.product_name);
-            });
-        }
     },
-    mounted(){
-        this.getJson(`${API + this.catalogUrl}`)
-            .then(data => {
-                for(let el of data){
-                    this.products.push(el);
-                    this.filteredGoods.push(el)
-                }
-            });
-        this.getJson(`getProducts.json`)
-            .then(data => {
-                for(let el of data){
-                    this.products.push(el);
-                    this.filteredGoods.push(el)
-                }
-            })
-
-
-    },
-    computed: {
-        divClasses() {
-            return {
-                invisible: !this.isVisible,
-                '': this.isVisible
-            }
-        }
+    mounted() {
+        console.log(this);
     }
-})
+});
+
